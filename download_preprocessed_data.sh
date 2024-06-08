02cc5031b2b216255cddcae40e157b60a869f879
Hayden Kwok <77424165+hakwok@users.noreply.github.com>
2024-06-07 23:49:40 -0700

    Update download_preprocessed_data.sh

--- download_preprocessed_data.sh
+++ download_preprocessed_data.sh
@@ -2,6 +2,6 @@
 
 mv data data_old
 
-wget https://nlp.stanford.edu/projects/myasu/QAGNN/data_preprocessed_release.zip
+curl -O https://nlp.stanford.edu/projects/myasu/QAGNN/data_preprocessed_release.zip
 unzip data_preprocessed_release.zip
 mv data_preprocessed_release  data
